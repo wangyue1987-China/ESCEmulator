65e571938e24882881d767d554cf64466f530381
Marcel Siegwart <marcel.siegwart@gmx.ch>
2022-08-21 15:16:34 +0200

    Start Implementation of ESC

--- ESCEmulatorWindowsDriver/ESC.c
+++ ESCEmulatorWindowsDriver/ESC.c
@@ -1,7 +1,52 @@
 #include "ESC.h"
-
-void ProcessFrame(PVOID frame, UINT32 lenght)
+LONG ESCAddEsc(ULONG PID)
 {
-	frame = frame;
-	lenght = lenght;
+	UNREFERENCED_PARAMETER(PID);
+	return -1;
+}
+LONG ESCRemoveEsc(ULONG PID)
+{
+	UNREFERENCED_PARAMETER(PID);
+	return -1;
+}
+LONG ESCGetEsc(ULONG Number)
+{
+	UNREFERENCED_PARAMETER(Number);
+	return -1;
+}
+LONG ESCSetRegisterAddress(USHORT Address)
+{
+	UNREFERENCED_PARAMETER(Address);
+	return -1;
+}
+LONG ESCWriteRegister(USHORT Len, PUSHORT Data)
+{
+	UNREFERENCED_PARAMETER(Len);
+	UNREFERENCED_PARAMETER(Data);
+	return -1;
+}
+LONG ESCReadRegister(USHORT Len, PUSHORT Data)
+{
+	UNREFERENCED_PARAMETER(Len);
+	UNREFERENCED_PARAMETER(Data);
+	return -1;
+}
+
+LONG ESCProcessFrame(PUCHAR Frame, ULONG Lenght)
+{
+	UNREFERENCED_PARAMETER(Lenght);
+	UCHAR tmp[6];
+	for(INT i = 0; i < 6;i++)
+	{
+		tmp[i] = Frame[i];
+	}
+	for (INT i = 0; i < 6; i++)
+	{
+		Frame[i] = Frame[i + 6];
+	}
+	for (INT i = 0; i < 6; i++)
+	{
+		Frame[i + 6] = tmp[i];
+	}
+	return -1;
 }
